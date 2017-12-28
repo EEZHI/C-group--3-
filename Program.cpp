9da67e92a254814059f011a1a3efaffc476da510
EEZHI <34395874+EEZHI@users.noreply.github.com>
2017-12-28 19:17:11 +0100

    Create Program.cpp

---
+++ Program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/* This Program is Going to display the multiplication of a postive number*/
+int main()
+{
+  int n;
+  cout<<"Enter a postive integer/number";
+  cin>>n;
+  
+  for (int i = 1 <= 10; i++) {
+          cout << n << " * " << i << " = " << n * i << endl;
+    }
+    
+    return 0;
+}    
